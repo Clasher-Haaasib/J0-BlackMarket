4a5df4cb94258c3bb54e480f7aaa40cc1d36e98e
Hasib <130287429+Haaasib@users.noreply.github.com>
2026-01-30 12:27:07 +0600

    Boilerplate Upload

--- shared/config.lua
+++ shared/config.lua
@@ -1,9 +1,9 @@
 Config = {
     FrameworkSettings = {
         CoreName = "qb-core", -- qb-core, es_extended, qbx_core, custom
-        EmailResource = "npwd", -- lb-phone, 17mov_Phone, qb-phone, npwd, CUSTOM  (fw-sv file check please)
+        EmailResource = "qb-phone", -- lb-phone, 17mov_Phone, qb-phone, npwd, CUSTOM  (fw-sv file check please)
         TargetSettings = {
-            resource = 'ox_target', --- ox_target | qb-target | interact |
+            resource = 'qb-target', --- ox_target | qb-target | interact |
             debug = false,
         },
     },
